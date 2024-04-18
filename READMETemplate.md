3195597b3e2f6e3b8d50b1c38dea8d1de50d7d49
@r0f33 <jiske.nicolson@gmail.com>
2024-04-18 18:09:42 +0930

    added base structure of fields to the generated README

---
+++ READMETemplate.md
@@ -0,0 +1,52 @@
+# Title
+
+## [The Deployed Page](https://awesome-foursome.github.io/boardgame-bliss/)
+
+## The Project
+
+## Link & Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Future Development](#future-development)
+- [Credits](#credits)
+- [Contributing](#contributing)
+- [Questions and Feedback](#questions-and-feedback)
+- [Tests](#tests)
+- [License](#license)
+
+## Image
+
+![Picture of the Homepage](./assets/images/homepage-deployment-screenshot.png)
+
+## Installation
+
+replace
+
+## Usage
+
+replace
+
+## Future Development
+
+replace
+
+## Credits
+
+replace
+
+## Contributing
+
+replace
+
+## Questions and Feedback
+
+replace
+
+## Tests
+
+replace
+
+## License
+
+replace
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const { writeFile } = require('fs').promises;
 
+// prompt user for README contents
 const getContents = () => {
     return inquirer.prompt([
         {
@@ -56,18 +57,64 @@ const getContents = () => {
     ]);
 };
 
-const writeREADME = ({ title, description, installation, usage, futureDevelopment, credits, contributing, questionsAndFeedback, tests, license }) =>
-    `${title}
-    ${description}
-    ${installation}
-    ${usage}
-    ${futureDevelopment}
-    ${credits}
-    ${contributing}
-    ${questionsAndFeedback}
-    ${tests}
-    ${license}`;
+// write contents from prompt to .md file
+const writeREADME = ({ title, description, installation, usage, futureDevelopment, credits, contributing, questionsAndFeedback, tests, license }) => `# ${title}
 
+## [The Deployed Page](https://awesome-foursome.github.io/boardgame-bliss/)
+
+## The Project
+
+${description}
+
+## Link & Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Future Development](#future-development)
+- [Credits](#credits)
+- [Contributing](#contributing)
+- [Questions and Feedback](#questions-and-feedback)
+- [Tests](#tests)
+- [License](#license)
+
+## Image
+
+![Picture of the Homepage](./assets/images/homepage-deployment-screenshot.png)
+
+## Installation
+
+${installation}
+
+## Usage
+
+${usage}
+
+## Future Development
+
+${futureDevelopment}
+
+## Credits
+
+${credits}
+
+## Contributing
+
+${contributing}
+
+## Questions and Feedback
+
+${questionsAndFeedback}
+
+## Tests
+
+${tests}
+
+## License
+
+${license}
+    `;
+
+// trigger prompt, contents generation and writing/creating .md file
 const generateREADME = () => {
     getContents()
         .then((contents) => writeFile('README.md', writeREADME(contents)))
