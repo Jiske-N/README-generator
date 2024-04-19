c5d8084da026e23ee94c0249b249b0bd9f75db1d
@r0f33 <jiske.nicolson@gmail.com>
2024-04-19 16:38:53 +0930

    added check to prompt resubmitting data if required fields are incomplete

--- index.js
+++ index.js
@@ -2,11 +2,23 @@ const { writeFile } = require('fs').promises;
 const getContents = require('./assets/scripts/getContents.js');
 const writeContents = require('./assets/scripts/writeContents.js');
 
+const checkResponse = ({ userName, repoName, title, license }) => {
+    if (!userName || !repoName || !title || !license) {
+        return "incomplete"
+    }
+};
+
 // trigger prompt, contents generation and writing/creating .md file
 const generateREADME = () => {
 
     // prompts to retrieve user inputs
     getContents()
+        .then((contents) => {
+            if (checkResponse(contents) === "incomplete") {
+                throw new Error('Incomplete user inputs. Try again.');
+            }
+            return contents;
+        })
 
         // writes
         .then((contents) => writeFile('README.md', writeContents(contents)))
